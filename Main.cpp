51938dd313c938c0b4a2fdbfacafcd04c262f408
Yara Kamal Aldeen <ykamaleddin@gmail.com>
2026-01-08 12:23:57 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -26,23 +26,11 @@ void drawCube(glm::mat4 model, glm::mat4 view, glm::mat4 proj, GLuint program, g
 
 void drawTree(glm::vec3 pos, glm::mat4 view, glm::mat4 proj, GLuint program)
 {
-    drawCube(
-        glm::scale(
-            glm::translate(glm::mat4(1.0f), pos),
-            glm::vec3(0.3f, 1.5f, 0.3f)
-        ),
-        view, proj, program,
-        { 0.4f, 0.25f, 0.1f, 1.0f }
-    );
+    drawCube(glm::scale(glm::translate(glm::mat4(1.0f), pos),glm::vec3(0.3f, 1.5f, 0.3f)),
+        view, proj, program,{ 0.4f, 0.25f, 0.1f, 1.0f });
 
-    drawCube(
-        glm::scale(
-            glm::translate(glm::mat4(1.0f), pos + glm::vec3(0, 1.5f, 0)),
-            glm::vec3(1.2f, 1.2f, 1.2f)
-        ),
-        view, proj, program,
-        { 0.2f, 0.6f, 0.2f, 1.0f }
-    );
+    drawCube(glm::scale(glm::translate(glm::mat4(1.0f), pos + glm::vec3(0, 1.3f, 0)),glm::vec3(1.2f, 1.2f, 1.2f)),
+        view, proj, program,{ 0.2f, 0.6f, 0.2f, 1.0f } );
 }
 
 int main()
@@ -191,8 +179,20 @@ int main()
                 drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-5, 1, 0) ), glm::vec3(0.05f, 2, doorW1)),
                     view, proj, program, { 0.4f,0.7f,0.9f,0.35f });
 
-                drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, 1, -3) ), glm::vec3(10, 2, 0.05f)),
-                    view, proj, program, { 0.8f,0.8f,0.85f,1 });
+                drawCube( glm::scale( glm::translate( glm::mat4(1),glm::vec3(-4.375f, 1.0f, -3.0f) ),glm::vec3(1.25f, 2.0f, 0.05f)  ),
+                    view, proj, program,{ 0.8f, 0.8f, 0.85f, 1 } );
+                drawCube(glm::scale(glm::translate( glm::mat4(1),glm::vec3(-1.5f, 1.0f, -3.0f)  ), glm::vec3(1.5f, 2.0f, 0.05f) ),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                drawCube(glm::scale( glm::translate( glm::mat4(1),glm::vec3(4.375f, 1.0f, -3.0f)), glm::vec3(1.25f, 2.0f, 0.05f)        ),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                drawCube(glm::scale( glm::translate(glm::mat4(1),glm::vec3(1.5f, 1.0f, -3.0f)),glm::vec3(1.5f, 2.0f, 0.05f) ),
+                    view, proj, program,{ 0.8f, 0.8f, 0.85f, 1 });
+                drawCube(glm::scale( glm::translate(glm::mat4(1), glm::vec3(-3.0f, 0.499f, -3.0f)),glm::vec3(1.5f, 1.01f, 0.05f)),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                drawCube(glm::scale( glm::translate(glm::mat4(1), glm::vec3(0.0f, 0.499f, -3.0f) ), glm::vec3(1.5f, 1.01f, 0.05f)  ),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                drawCube( glm::scale( glm::translate( glm::mat4(1), glm::vec3(3.0f, 0.499f, -3.0f) ),glm::vec3(1.5f, 1.01f, 0.05f) ),
+                    view, proj, program,{ 0.8f, 0.8f, 0.85f, 1 } );
 
 
 
@@ -201,11 +201,11 @@ int main()
                     view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
 
                 drawCube( glm::scale( glm::translate( glm::mat4(1), glm::vec3(0, 2.4875f, -3) ), glm::vec3(10, 0.9f, 0.05f) ),
-                    view, proj, program,{ 0.4f, 0.7f, 0.9f, 0.35f } );
+                    view, proj, program, { 0.6f,0.6f,0.6f,1 });
 
 
                 drawCube( glm::scale( glm::translate( glm::mat4(1),glm::vec3(-5, 2.4875f, 0) ),glm::vec3(0.05f, 0.9f, 4.0f + doorW1) ),
-                    view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f } );
+                    view, proj, program, { 0.6f,0.6f,0.6f,1 });
 
 
                 drawCube( glm::scale( glm::translate( glm::mat4(1), glm::vec3(-5, -0.4875f, 0)  ),glm::vec3(0.05f, 0.9f, 4.0f + doorW1) ),
@@ -213,14 +213,14 @@ int main()
 
 
                 drawCube(glm::scale( glm::translate( glm::mat4(1),glm::vec3(5, 2.4875f, 0)), glm::vec3(0.05f, 0.9f, 6.0f) ),
-                    view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
+                    view, proj, program, { 0.6f,0.6f,0.6f,1 });
 
                 drawCube( glm::scale(glm::translate( glm::mat4(1), glm::vec3(5, -0.4875f, 0)  ), glm::vec3(0.05f, 0.9f, 6.0f)),
                     view, proj,program,{ 0.4f, 0.7f, 0.9f, 0.35f });
 
 
                 drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, 2.4875f, 3) ), glm::vec3(10.0f, 0.9f, 0.05f) ),
-                    view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
+                    view, proj, program, { 0.6f,0.6f,0.6f,1 });
 
                     drawCube( glm::scale( glm::translate(glm::mat4(1), glm::vec3(0, -0.4875f, 3) ), glm::vec3(10.0f, 0.9f, 0.05f) ),
                     view, proj, program,{ 0.4f, 0.7f, 0.9f, 0.35f });
@@ -275,26 +275,42 @@ int main()
                 drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-5, 1, 0) + glm::vec3(10, 0, 0)), glm::vec3(0.05f, 2, doorW2)),
                     view, proj, program, { 0.4f,0.7f,0.9f,0.35f });
 
-                drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, 1, -3) + glm::vec3(10, 0, 0)), glm::vec3(10, 2, 0.05f)),
-                    view, proj, program, { 0.8f,0.8f,0.85f,1 });
-
-
+                drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-4.375f, 1.0f, -3.0f) + glm::vec3(10, 0, 0)), glm::vec3(1.25f, 2.0f, 0.05f)),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-1.5f, 1.0f, -3.0f) + glm::vec3(10, 0, 0)), glm::vec3(1.5f, 2.0f, 0.05f)),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(4.375f, 1.0f, -3.0f) + glm::vec3(10, 0, 0)), glm::vec3(1.25f, 2.0f, 0.05f)),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(1.5f, 1.0f, -3.0f) + glm::vec3(10, 0, 0)), glm::vec3(1.5f, 2.0f, 0.05f)),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-3.0f, 0.499f, -3.0f) + glm::vec3(10, 0, 0)), glm::vec3(1.5f, 1.01f, 0.05f)),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0.0f, 0.499f, -3.0f) + glm::vec3(10, 0, 0)), glm::vec3(1.5f, 1.01f, 0.05f)),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(3.0f, 0.499f, -3.0f) + glm::vec3(10, 0, 0)), glm::vec3(1.5f, 1.01f, 0.05f)),
+                    view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
 
 
                 drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, -0.49f, -3) + glm::vec3(10, 0, 0)), glm::vec3(10, 0.9f, 0.05f)),
                     view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
+
                 drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, 2.4875f, -3) + glm::vec3(10, 0, 0)), glm::vec3(10, 0.9f, 0.05f)),
-                    view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
+                    view, proj, program, { 0.6f,0.6f,0.6f,1 });
+
+
+                drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-5, 2.4875f, 0) + glm::vec3(10, 0, 0)), glm::vec3(0.05f, 0.9f, 4.0f + doorW1)),
+                    view, proj, program, { 0.6f,0.6f,0.6f,1 });
+
 
                 drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(5, 2.4875f, 0) + glm::vec3(10, 0, 0)), glm::vec3(0.05f, 0.9f, 6.0f)),
-                    view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
+                    view, proj, program, { 0.6f,0.6f,0.6f,1 });
 
                 drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(5, -0.4875f, 0) + glm::vec3(10, 0, 0)), glm::vec3(0.05f, 0.9f, 6.0f)),
                     view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
 
 
                 drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, 2.4875f, 3) + glm::vec3(10, 0, 0)), glm::vec3(10.0f, 0.9f, 0.05f)),
-                    view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
+                    view, proj, program, { 0.6f,0.6f,0.6f,1 });
 
                 drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, -0.4875f, 3) + glm::vec3(10, 0, 0)), glm::vec3(10.0f, 0.9f, 0.05f)),
                     view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
@@ -349,31 +365,49 @@ int main()
                     drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-5, 1, 0) + glm::vec3(20, 0, 0)), glm::vec3(0.05f, 2, doorW3)),
                         view, proj, program, { 0.4f,0.7f,0.9f,0.35f });
 
-                    drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, 1, -3) + glm::vec3(20, 0, 0)), glm::vec3(10, 2, 0.05f)),
-                        view, proj, program, { 0.8f,0.8f,0.85f,1 });
+                    drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-4.375f, 1.0f, -3.0f) + glm::vec3(20, 0, 0)), glm::vec3(1.25f, 2.0f, 0.05f)),
+                        view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                    drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-1.5f, 1.0f, -3.0f) + glm::vec3(20, 0, 0)), glm::vec3(1.5f, 2.0f, 0.05f)),
+                        view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                    drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(4.375f, 1.0f, -3.0f) + glm::vec3(20, 0, 0)), glm::vec3(1.25f, 2.0f, 0.05f)),
+                        view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                    drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(1.5f, 1.0f, -3.0f) + glm::vec3(20, 0, 0)), glm::vec3(1.5f, 2.0f, 0.05f)),
+                        view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                    drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-3.0f, 0.499f, -3.0f) + glm::vec3(20, 0, 0)), glm::vec3(1.5f, 1.01f, 0.05f)),
+                        view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                    drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0.0f, 0.499f, -3.0f) + glm::vec3(20, 0, 0)), glm::vec3(1.5f, 1.01f, 0.05f)),
+                        view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
+                    drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(3.0f, 0.499f, -3.0f) + glm::vec3(20, 0, 0)), glm::vec3(1.5f, 1.01f, 0.05f)),
+                        view, proj, program, { 0.8f, 0.8f, 0.85f, 1 });
 
 
 
 
 
+              
                     drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, -0.49f, -3) + glm::vec3(20, 0, 0)), glm::vec3(10, 0.9f, 0.05f)),
                         view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
+
                     drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, 2.4875f, -3) + glm::vec3(20, 0, 0)), glm::vec3(10, 0.9f, 0.05f)),
-                        view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
+                        view, proj, program, { 0.6f,0.6f,0.6f,1 });
+
+
+                    drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(-5, 2.4875f, 0) + glm::vec3(20, 0, 0)), glm::vec3(0.05f, 0.9f, 4.0f + doorW1)),
+                        view, proj, program, { 0.6f,0.6f,0.6f,1 });
+
 
                     drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(5, 2.4875f, 0) + glm::vec3(20, 0, 0)), glm::vec3(0.05f, 0.9f, 6.0f)),
-                        view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
+                        view, proj, program, { 0.6f,0.6f,0.6f,1 });
 
                     drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(5, -0.4875f, 0) + glm::vec3(20, 0, 0)), glm::vec3(0.05f, 0.9f, 6.0f)),
                         view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
 
 
                     drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, 2.4875f, 3) + glm::vec3(20, 0, 0)), glm::vec3(10.0f, 0.9f, 0.05f)),
-                        view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
+                        view, proj, program, { 0.6f,0.6f,0.6f,1 });
 
                     drawCube(glm::scale(glm::translate(glm::mat4(1), glm::vec3(0, -0.4875f, 3) + glm::vec3(20, 0, 0)), glm::vec3(10.0f, 0.9f, 0.05f)),
                         view, proj, program, { 0.4f, 0.7f, 0.9f, 0.35f });
-
                     glm::vec3 windows3[] = {
                         {-3,1.5f,-2.975f},{0,1.5f,-2.975f},{3,1.5f,-2.975f}
                     };
@@ -398,55 +432,25 @@ int main()
 
                     drawRooms();
 
-                    drawCube(
-                        glm::scale(
-                            glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, -1.05f, 10.0f)),
-                            glm::vec3(60.0f, 0.05f, 12.0f)
-                        ),
-                        view, proj, program,
-                        { 0.15f, 0.15f, 0.15f, 1.0f }
-                    );
+                    drawCube(glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, -1.05f, 10.0f)),glm::vec3(60.0f, 0.05f, 12.0f)),
+                        view, proj, program,{ 0.15f, 0.15f, 0.15f, 1.0f });
 
 
-                    drawCube(
-                        glm::scale(
-                            glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, -1.0f, 4.5f)),
-                            glm::vec3(40.0f, 0.1f, 1.0f)
-                        ),
-                        view, proj, program,
-                        { 0.6f, 0.6f, 0.6f, 1.0f }
-                    );
+                    drawCube(glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, -1.0f, 4.5f)),glm::vec3(40.0f, 0.1f, 1.0f)),
+                        view, proj, program,{ 0.6f, 0.6f, 0.6f, 1.0f });
 
-                    drawCube(
-                        glm::scale(
-                            glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, -1.0f, 18.0f)),
-                            glm::vec3(40.0f, 0.1f, 1.0f)
-                        ),
-                        view, proj, program,
-                        { 0.6f, 0.6f, 0.6f, 1.0f }
-                    );
+                    drawCube(glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, -1.0f, 18.0f)),glm::vec3(40.0f, 0.1f, 1.0f)),
+                        view, proj, program,{ 0.6f, 0.6f, 0.6f, 1.0f } );
 
 
-                    drawCube(
-                        glm::scale(
-                            glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, -1.0f, 3.51f)),
-                            glm::vec3(40.0f, 0.1f, 0.98f)
-                        ),
-                        view, proj, program,
-                        { 0.2f, 0.6f, 0.2f, 0.7f }
-                    );
+                    drawCube(glm::scale( glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, -1.0f, 3.51f)),glm::vec3(40.0f, 0.1f, 0.98f)),
+                        view, proj, program,{ 0.2f, 0.6f, 0.2f, 0.7f } );
 
-                    drawCube(
-                        glm::scale(
-                            glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, -1.0f, 20.5f)),
-                            glm::vec3(40.0f, 0.1f, 4.0f)
-                        ),
-                        view, proj, program,
-                        { 0.2f, 0.6f, 0.2f, 0.7f }
-                    );
+                    drawCube(glm::scale( glm::translate(glm::mat4(1.0f), glm::vec3(10.0f, -1.0f, 20.5f)),glm::vec3(40.0f, 0.1f, 4.0f)),
+                        view, proj, program,{ 0.2f, 0.6f, 0.2f, 0.7f });
 
 
-                    for (float x = -6.0f; x <= 35.0f; x += 4.0f)
+                    for (float x = -6.5f; x <= 35.0f; x += 4.0f)
                     {
                         drawTree(glm::vec3(x, -1.0f, 3.6f), view, proj, program);
                         drawTree(glm::vec3(x, -1.0f, 20.0f), view, proj, program);
